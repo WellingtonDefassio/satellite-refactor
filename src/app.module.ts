d9c5ec00c3d6780652b36a3cdab1aafec6bfb9b0
Wellington Defassio <well.defassio@gmail.com>
2022-07-29 14:45:23 -0300

    feat: create satellite folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoService } from './mo/mo.service';
+import { SatelliteModule } from './satellite/satellite.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [SatelliteModule],
+  controllers: [],
+  providers: [MoService],
 })
 export class AppModule {}
