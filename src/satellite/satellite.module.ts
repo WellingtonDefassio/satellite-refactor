e56e94120d26a7f7cd23a9d1c5e34eb20d1f8828
Wellington Defassio <well.defassio@gmail.com>
2022-07-29 15:04:55 -0300

    feat: create orbcomm folders

--- src/satellite/satellite.module.ts
+++ src/satellite/satellite.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SatelliteService } from './satellite.service';
 import { SatelliteController } from './satellite.controller';
+import { OrbcommV2Service } from './api/orbcomm_v2/orbcomm_v2.service';
+import { OrbcommV2Service } from './api/orbcomm-v2/orbcomm-v2.service';
+import { OrbcommV2Controller } from './api/orbcomm-v2/orbcomm-v2.controller';
+import { DownloadService } from './api/orbcomm-v2/download/download.service';
+import { OrbcommV2Module } from './api/orbcomm-v2/orbcomm-v2.module';
 
 @Module({
-  providers: [SatelliteService],
-  controllers: [SatelliteController]
+  providers: [SatelliteService, OrbcommV2Service, DownloadService],
+  controllers: [SatelliteController, OrbcommV2Controller],
+  imports: [OrbcommV2Module]
 })
 export class SatelliteModule {}
