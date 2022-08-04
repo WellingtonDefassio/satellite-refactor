c5c6f6d837c2f8da29dba5f9e0fa06383ee7c1f0
Wellington Defassio <well.defassio@gmail.com>
2022-08-04 14:55:51 -0300

    commit

--- src/satellite/satellite.service.ts
+++ src/satellite/satellite.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { OrbcommDownloadMessages } from '@prisma/client';
 import { ArgumentOutOfRangeError, filter } from 'rxjs';
 import { SendMessageDto } from '../dtos/satellite.dto';
 import { PrismaService } from '../prisma/prisma.service';
@@ -20,13 +21,35 @@ export class SatelliteService {
   async downloadMessagesAll(param: QueryDownloadParamsDto) {
     const { limit, device, startDate, mobileId } = param;
 
-    return await this.prisma.orbcommDownloadMessages.findMany({
-      take: limit,
-      where: {
-        deviceId: device,
-        messageUTC: { gte: startDate },
-        mobileOwnerID: mobileId,
-      },
-    });
+    const downloadMessagesList =
+      await this.prisma.orbcommDownloadMessages.findMany({
+        where: {
+          deviceId: device,
+          messageUTC: { gte: startDate },
+          mobileOwnerID: mobileId,
+        },
+        take: limit,
+      });
+
+    if (!downloadMessagesList.length) {
+      return downloadMessagesList;
+      //TODO tratar o retorno de resultados vazios
+    }
+    return downloadMessagesList;
+  }
+
+  nextMessageToFind(downloadMessagesList: OrbcommDownloadMessages[]) {
+    const result = downloadMessagesList.sort(
+      (data, data2) =>
+        new Date(data.messageUTC).getTime() -
+        new Date(data2.messageUTC).getTime(),
+    );
+    const lastRegister = result[result.length - 1];
+
+    console.log(lastRegister);
+    const { messageUTC } = lastRegister;
+    const date = messageUTC.setSeconds(messageUTC.getSeconds() + 1);
+
+    console.log(new Date(date));
   }
 }
